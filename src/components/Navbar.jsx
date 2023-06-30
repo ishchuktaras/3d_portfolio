2587318a5a04297e38cbc6c41930fefd51524e9b
Taras Ishchuk <ishchuktaras@gmail.com>
2023-06-30 22:16:42 +0200

    Main navbar links styling, Logo styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,20 +7,42 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  const [active, setActive] = useState('')
+  const [active, setActive] = useState("");
 
   return (
-    <nav className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav
+      className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link to="/" className="flex items-center gap-2" onClick={() => { setActive(""); window.scrollTo(0, 0); }}>
-          <img src={logo} alt="logo" className="w-12 h-12 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">Taras Ishchuk <span className="sm:block hidden"> | Web developer</span></p>
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-20 h-20 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Taras Ishchuk{" "}
+            <span className="sm:block hidden"> | Web developer</span>
+          </p>
         </Link>
-
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((Link) => (
+            <li
+              key={Link.id}
+              className={`${active === Link.title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(Link.title)}
+            >
+              <a href={`#${Link.id}`}>{Link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
-
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
