d9868326f26ff30a5a1c69e9109d4c04cb137309
Taras Ishchuk <ishchuktaras@gmail.com>
2023-06-30 21:00:26 +0200

    Tailwind install, logo description editing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,8 +12,9 @@ const Navbar = () => {
   return (
     <nav className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link to="/" className="flex items-center gap-2" onClick={() => { setActive(""); window.scrollTo(0,0);}}>
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+        <Link to="/" className="flex items-center gap-2" onClick={() => { setActive(""); window.scrollTo(0, 0); }}>
+          <img src={logo} alt="logo" className="w-12 h-12 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">Taras Ishchuk <span className="sm:block hidden"> | Web developer</span></p>
         </Link>
 
       </div>
