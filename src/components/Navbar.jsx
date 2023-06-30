38d29c15e53a978ac7ce90b61496ff36a5dc15f9
Taras Ishchuk <ishchuktaras@gmail.com>
2023-06-30 22:30:11 +0200

    Menu icon adding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav
@@ -40,6 +41,14 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer" 
+            onClick={() => setToggle(!toggle)}
+          />
+        </div>
       </div>
     </nav>
   );
