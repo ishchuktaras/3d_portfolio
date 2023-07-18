5d99d79f43e9b84acd0b7488ef3b08900ea15af1
Taras Ishchuk <ishchuktaras@gmail.com>
2023-07-18 21:28:57 +0200

    Texture Canvas edit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,19 +11,19 @@ const Computers = ({ isMobile }) => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
+        position={[-145, 15, 45]}
+        angle={-1}
         penumbra={1}
-        intensity={1}
+        intensity={0.04}
         castShadow
         shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 1 : 2}
+        position={isMobile ? [0, -3, -2.2] : [0.2, -3.7, -0.2]}
+        rotation={[0, 0, -0.30]}
       />
     </mesh>
   );
@@ -58,7 +58,7 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [25, 9, 9], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
